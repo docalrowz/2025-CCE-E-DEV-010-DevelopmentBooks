d0260f7a122e28eab30e1943159eb81b381a3a97
docalrowz <55087764+docalrowz@users.noreply.github.com>
2025-10-15 23:51:13 +0200

    add minimal PricingService implementation so tests can run (expected to fail)

--- src/main/java/com/kata/books/service/PricingService.java
+++ src/main/java/com/kata/books/service/PricingService.java
@@ -1,4 +1,13 @@
 package com.kata.books.service;
 
+import com.kata.books.model.BookTitle;
+import java.math.BigDecimal;
+import java.util.Map;
+
 public class PricingService {
+
+    //For test purpose
+    public BigDecimal price(Map<BookTitle, Integer> basket) {
+        return new BigDecimal("0.00");
+    }
 }
